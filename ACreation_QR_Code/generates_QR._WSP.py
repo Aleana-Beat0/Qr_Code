393e2fc6c69cbcfde74260b5adc4ddfb09cb89d4
Aleana-Beat0 <adbeato19@gmail.com>
2025-01-31 01:23:25 +0800

    📝 Edited WSP real quick

--- ACreation_QR_Code/generates_QR._WSP.py
+++ ACreation_QR_Code/generates_QR._WSP.py
@@ -1,21 +1,20 @@
+#Generates the QR without the special code
+
 
 import pandas as pd 
+#make sure to import creator 
 from creator import createqr
 
-
-
-# Read a comma separated values (CSV) files into a variable
-# as a pandas DataFrame
-
-
+#File where the names are being saved
 sheet=pd.read_excel("sheet.xlsx", "Google sheet")
+
 
 for index, r in sheet.iterrows():
     names = r['Full Name']
     approved_names = names
     print(approved_names)
     
-    #createqr(approved_names,'0')
+    createqr(approved_names,'')
 
 
 
--- ACreation_QR_Code/genqr_streamlit.py
+++ ACreation_QR_Code/genqr_streamlit.py
@@ -1,3 +1,6 @@
+#Current web based for generating the qr code -- work in progress
+
+
 import streamlit as st
 import pandas as pd
 import tempfile
